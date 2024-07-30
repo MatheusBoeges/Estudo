41f397c4793d92cba6393ea4b9143ca7e5f4f763
MatheusBorges <matheus.s.bcorrea@gamil.com>
2024-07-30 20:19:12 -0300

    Modificando a classe ProdutoTeste.

--- Classe/Desafio/ProdutoTeste.java
+++ Classe/Desafio/ProdutoTeste.java
@@ -4,13 +4,13 @@ public class ProdutoTeste {
 	
 	public static void main(String[] args) {
 		
-		Produto p1 = new Produto("Notebook", 4356.89);
+		Produto1 p1 = new Produto1("Notebook", 4356.89);
 		
-		var p2 = new Produto();
+		var p2 = new Produto1();
 		p2.nome = "Caneta Preta";
 		p2.preco = 12.56;
 		
-		Produto.desconto = 0.50;
+		Produto1.desconto = 0.50;
 		
 		System.out.println(p1.nome + " " + p1.precoComDesconto());
 		System.out.println(p2.nome + " " + p2.precoComDesconto());
